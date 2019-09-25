b86ea8e2d51dbcb27861b2dcf61457fc2bbc1b6c
hamidurme09 <hamidur.me09@gmail.com>
2019-09-25 12:00:03 +0600

    Updates

---
+++ precache-manifest.dd5bc71ff075d17af814c27f5e2ef610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b057be834ceb49a55f34ca2ca9c1379",
+    "url": "/rabbit/index.html"
+  },
+  {
+    "revision": "58827fd975c5ca062520",
+    "url": "/rabbit/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "0bba40e85a9974ac6a19",
+    "url": "/rabbit/static/js/2.96af93cd.chunk.js"
+  },
+  {
+    "revision": "58827fd975c5ca062520",
+    "url": "/rabbit/static/js/main.8012a1d6.chunk.js"
+  },
+  {
+    "revision": "d8d621318fcecdde19e6",
+    "url": "/rabbit/static/js/runtime-main.45f46293.js"
+  }
+]);
